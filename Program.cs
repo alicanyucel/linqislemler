d61b4e5abc3593197962463d257d56dc03b633a9
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2022-04-30 12:18:19 +0300

    Linqİslemler

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]  liste = new int[6] { 1, 3, 33, 44, 56, 101 };
+
+            Console.WriteLine("tekler");
+            var tekler =
+                         from sayi in liste
+                         where sayi % 2 == 1
+                         select sayi;
+            foreach (var tekSayi in tekler)
+            {
+                Console.WriteLine(tekSayi);
+            }
+
+            Console.WriteLine("ciftler");
+            var ciftler=from sayi in liste
+                        where sayi % 2==0
+                        select sayi;
+
+            foreach (var ciftSayi in ciftler)
+            {
+                Console.WriteLine(ciftSayi);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
